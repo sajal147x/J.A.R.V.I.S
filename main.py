7114b11e930782d36d49ab523f8fed077b2785d8
sajal gupta <sajal147x@gmail.com>
2026-08-15 14:31:01 -0500

    add verbose flag for metadata

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from tokenize import String
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -11,7 +12,9 @@ def main():
     #PARSING USER INPUT
     parser = argparse.ArgumentParser(description="chatbot")
     parser.add_argument("user_prompt", type=str, help="User Prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
     #CONFIG
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
@@ -25,18 +28,22 @@ def main():
         }
     ]
     #API CALL TO THE LLM
-    generate_content(client, messages)
+    generate_content(client, messages, args.verbose, args.user_prompt)
 
 
-def generate_content(client: OpenAI, messages: list[ChatCompletionMessageParam]) -> None:
+def generate_content(client: OpenAI, messages: list[ChatCompletionMessageParam], verbose : bool = False, userPrompt: str = "") -> None:
     response = client.chat.completions.create(model="openrouter/free", messages=messages)
     usage = response.usage
     #HANDLING NULL CASE
     if usage == None:
         raise RuntimeError("Failed API Request")
     #OUTPUT DETAILS
-    print("Prompt tokens: ", usage.prompt_tokens)
-    print("Response tokens: ", usage.completion_tokens)
+    # VERBOSE FIRST
+    if verbose:
+        print("User prompt: ", userPrompt)
+        print("Prompt tokens: ", usage.prompt_tokens)
+        print("Response tokens: ", usage.completion_tokens)
+
     print(response.choices[0].message.content)
 
 
