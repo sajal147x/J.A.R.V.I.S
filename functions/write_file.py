0902beea4960dc6995129e1bc26a8d98fb671c18
sajal gupta <sajal147x@gmail.com>
2026-08-17 14:03:12 -0500

    adding functionality to write to a file and tests

---
+++ functions/write_file.py
@@ -0,0 +1,21 @@
+import os
+
+def write_file(working_directory: str, file_path: str, content: str) -> str:
+    try:
+        # CREATING PATH AND VALIDATION
+        absPath = os.path.abspath(working_directory)
+        fullPath = os.path.join(absPath, file_path)
+        targetFile = os.path.normpath(fullPath)
+        validTargetFile = os.path.commonpath([absPath, targetFile]) == absPath
+        if not validTargetFile:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if  os.path.isdir(targetFile):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        os.makedirs(os.path.dirname(targetFile), exist_ok=True)
+        ####
+        with open(targetFile, "w") as f:
+            result = f.write(content)
+        return f'Successfully wrote to "{file_path}" ({result} characters written)'
+    except Exception as e:
+        message = str(e)
+        return f'Error: Internal Server Error {message}'
